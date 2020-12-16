fb8d7d99a9ba88ca470e221666b269586b171ba4
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-12-16 14:05:29 +0100

    update on return value without ?

--- src/Service/Converter.php
+++ src/Service/Converter.php
@@ -7,21 +7,33 @@ namespace App\Service;
 class Converter
 {
 
-    public function celciustoFar(float $value) : ? float {
+    /**
+     * @param float $value
+     * @return float|null
+     */
+    public function celciustoFar(float $value) : float {
         $fahrenheit = $value - 32;
         $result = $fahrenheit / 1.8;
 
         return $result;
     }
 
-    public function meterToStep(float $value) : ? float {
+    /**
+     * @param float $value
+     * @return float|null
+     */
+    public function meterToStep(float $value) : float {
 
         $result = $value * 3.2808;
 
         return $result;
     }
 
-    public function kiloTolivre( float $value) : ? float {
+    /**
+     * @param float $value
+     * @return float|null
+     */
+    public function kiloTolivre(float $value) : float {
 
         $result = $value * 2.205;
 
