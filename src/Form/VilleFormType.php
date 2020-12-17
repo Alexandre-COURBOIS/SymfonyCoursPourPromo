4097505ce89e12a6001860860572217c60472643
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-12-17 16:46:42 +0100

    add front verif on formtype

--- src/Form/VilleFormType.php
+++ src/Form/VilleFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Ville;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +14,11 @@ class VilleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('codeCommune')
-            ->add('gentile')
-            ->add('recordTempChaleur')
-            ->add('recordTempFroid')
+            ->add('nom', TextType::class)
+            ->add('codeCommune',IntegerType::class)
+            ->add('gentile',TextType::class)
+            ->add('recordTempChaleur', IntegerType::class)
+            ->add('recordTempFroid', IntegerType::class)
         ;
     }
 
